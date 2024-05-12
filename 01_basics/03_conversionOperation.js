0e1d3321350626da1b56c560eadacac2884c4633
chishticodes <82635100+chishticodes@users.noreply.github.com>
2024-05-12 08:27:09 +0000

    prefix and postfix operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = 33
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let score1 = "33"
-console.log(typeof score1);
+// console.log(typeof score1);
 
 let valueInNumber = Number(score1)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 
 // '33' => 33
@@ -16,9 +16,25 @@ console.log(typeof valueInNumber);
 
 let isLoggedIn = 0;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 let someNumber = 33;
 let stringSomeNumber = String(someNumber)
-console.log(typeof stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+
+
+// *********************************OPERATIONS*********************************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+
+let age = 18
+console.log(++age);
+console.log(age++);
+console.log(age);
+
+

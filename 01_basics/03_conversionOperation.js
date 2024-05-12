f3d434a5e78b0367cedc2ab35059f60ffdde41c9
chishticodes <82635100+chishticodes@users.noreply.github.com>
2024-05-12 07:57:35 +0000

    type conversion opertations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 33
+
+console.log(typeof score);
+
+let score1 = "33"
+console.log(typeof score1);
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber);
+
+
+// '33' => 33
+// '33abc' => NaN
+// true => 1, false => 0 
+// "" => false
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33;
+let stringSomeNumber = String(someNumber)
+console.log(typeof stringSomeNumber);

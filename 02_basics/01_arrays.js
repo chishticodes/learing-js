a6462df664b49394d2f9f229f18e19e9a73a1692
chishticodes <82635100+chishticodes@users.noreply.github.com>
2024-05-23 05:50:45 +0000

    01 array done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -34,5 +34,11 @@ const newArr = myArr.join(); //it converts array in string
 
 console.log("A", myArr);
 
-const myn1 = myHeros.slice(1,3);
+const myn1 = myArr.slice(1,3);
 console.log(myn1);
+console.log("B", myArr);
+
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+console.log("C", myArr);

119c8c2883659efbb045ec17953b10639145fdea
chishticodes <82635100+chishticodes@users.noreply.github.com>
2024-05-30 05:36:29 +0000

    half loops done array with  loops are remaining

---
+++ 05_iterations/01_one.js
@@ -0,0 +1,15 @@
+// for
+
+// for(let i=1; i<11; i++){
+//     if(i == 5){
+//         console.log("5 is the best number");
+//     }
+//     console.log(i);
+// }
+
+for(let i = 0; i <= 10; i++){
+    console.log(`outer loop value: ${i}`);
+    for (let j = 0; j < 10; j++) {
+        console.log(`inner loop value: ${j} and outer loop value is ${i} `);
+    }
+}
